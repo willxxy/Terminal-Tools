090a0cb456934b5ef0e68f0aa9b00351d3f348ef
willxxy <90741489+willxxy@users.noreply.github.com>
2023-04-24 21:40:39 -0700

    init commit

---
+++ tools.sh
@@ -0,0 +1,20 @@
+# Return memory usage of current dir
+du -hs
+
+# Remote transfer folder/file to specified address
+path_to_folder=
+name_of_folder=
+tar_type=
+scp ${name_of_folder}.tar.${tar_type} username@IP_address:${path_to_folder}
+
+# Compress by tar
+name_of_compressed_file=
+tar -czvf ${name_of_compressed_file}.tar.gz ${path_to_folder}
+
+# Extract tar file
+tar -xf ${name_of_compressed_file}.tar.${tar_type}
+
+# Export conda environemnt while in environment
+name_of_env=
+conda env export --no-builds > ${name_of_env}.yml
+
