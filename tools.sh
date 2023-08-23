effd65ad7bffbe350b0c0b2d1cabd0fa4404f690
willxxy <90741489+willxxy@users.noreply.github.com>
2023-08-23 18:31:39 -0400

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -47,3 +47,5 @@ conda create -n myenv python=3.9
 pip install --user ipykernel
 python -m ipykernel install --user --name=myenv
 
+# Pytorch that works with A5000 and A6000 GPUs
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
