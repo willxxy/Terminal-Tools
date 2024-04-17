d354a994446c32c29d93431a76ab76e6fa7c03e3
willxxy <90741489+willxxy@users.noreply.github.com>
2024-04-17 02:32:01 -0400

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -4,3 +4,17 @@ tmux kill-session -t session_name
 
 # Create new tmux session with a specified name
 tmux new-session -s session_name
+
+# Create arbitrary tmux session
+tmux
+
+# attach to previously defined session
+tmux attach -t session_name
+
+# Note + is combination of keys
+# exit out of tmux
+control + b + d
+
+# scroll in tmux
+control + b + [
+
