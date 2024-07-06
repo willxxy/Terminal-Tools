432fb5965659535e79fa6b81cead0d7180eac1db
willxxy <90741489+willxxy@users.noreply.github.com>
2024-07-06 14:16:25 -0400

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -3,3 +3,9 @@ nvidia-smi
 
 ### Look at GPU usage, PID etc. in real time
 nvidia-smi -l
+
+### Print info about Cuda compiler driver
+nvcc --version
+
+### Print GPUs compute capbability
+nvidia-smi --query-gpu=compute_cap --format=csv
