6f0c33cc48a35864275ec3fbaa62742704e5a8bf
willxxy <90741489+willxxy@users.noreply.github.com>
2025-01-10 11:36:34 -0500

    Update system.sh

--- system.sh
+++ system.sh
@@ -16,3 +16,17 @@ cat /var/log/Xorg.0.log
 
 # crash logs
 dmesg | tail
+
+# checking SWAP mem usage by user and PID
+
+for user in $(ps -ef | awk '{print $1}' | sort | uniq); do
+    echo "User: $user"
+    ps -u $user -o pid,vsz,rss,command | awk '
+        NR > 1 {
+            vsz=$2
+            rss=$3
+            swap=(vsz-rss)/1024
+            if(swap>0)
+                print "  PID: "$1" Swap: "swap" MB Command: "$4
+        }'
+done
